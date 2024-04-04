c77d725e1239335411cefbb7540b505675de6f49
taufan.d <taufan.d@wistkey.dev>
2024-04-04 14:29:42 +0700

    add number 3

---
+++ number_3/main.go
@@ -0,0 +1,33 @@
+package main
+
+type Users struct {
+	Name String `json:"name"`
+	Email String `json:"email"`
+	Age int64 `json:"age"`
+}
+
+func main () {
+	// read json file 
+
+	jsonFile, err := os.Open("test3.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+
+	jsonByte, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var user Users
+	json.Unmarshal(jsonByte, &user)
+
+	user.Email = "johndoe@example.com"
+	user.Age++
+
+	userJson, _ := json.Marshal(user)
+	err = ioutil.WriteFile("test3.json", userJson, 0644)
+
+
+}
